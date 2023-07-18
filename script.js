e6f422e3a469efdb3da0ef838e2c3d5396e010f7
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-18 12:59:52 -0600

    bind method coding

--- script.js
+++ script.js
@@ -164,5 +164,40 @@ book.call(swiss, ...flightData);
 
 /*==============THE BIND METHOD ==============*/
 
- The bind Method
- book.call(eurowings, 23, 'Sarah Williams');
+//  The bind Method
+//  book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(delta);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Bravo');
+bookEW23('Mike SMith');
+
+// With event listeners
+
+//Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+//challenge
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+/*==============CODING CHALLENGE 1 ==============*/
