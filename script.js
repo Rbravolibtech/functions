93b6f40f65103ae9970ca98563951899caf72a69
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-18 11:18:21 -0600

    value vs reference coding done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*============== DEFAULT PARAMETERS ==============/*
 const bookings = [];
@@ -28,4 +28,35 @@ createBooking("LH123", 5);
 
 createBooking("LH123", undefined, 1000);
 
+/*==============HOW PASSING ARGUMENTS WORKS: VALUE VS REFERENCE ==============*/
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Bravo',
+//   passport: 159357147369,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH99';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 159357147369) {
+//     alert('Checked in');
+//   } else {
+//     alert('wrong passport!');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // is the same as doing...
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
 /*==============HOW PASSING ARGUMENTS WORKS: VALUE VS REFERENCE ==============*/
