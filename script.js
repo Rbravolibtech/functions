79239f5e2f4ed6dabc36889e014091a1b0c7bb4c
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-18 13:44:13 -0600

    closures code

--- script.js
+++ script.js
@@ -297,3 +297,20 @@ runOnce();
 console.log(notPrivate);
 
 /*==============CLOSURES==============*/
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
